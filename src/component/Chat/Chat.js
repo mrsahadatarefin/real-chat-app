b062c0c7d5c5c5327801213c44b6bce3ac184350
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-03-21 23:53:24 +0600

    message add

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react';
 import {user} from "../Join/Join"
 import socketIo from 'socket.io-client'
 import './Chat.css'
+import ReactScrollTOBottom from 'react-scroll-to-bottom'
+
 import sendLogo from '../../images/send.png'
+import Message from '../message/Message';
 const ENDPOINR ='http://localhost:5000/'
 
 let socket;
 const Chat = () => {
     const [id,setId]=useState('')
+    const [messages,setMessages]= useState([])
 
     const send = ()=>{
        const message= document.getElementById('chatInput').value;
@@ -26,12 +30,16 @@ const Chat = () => {
         socket.emit('joined',{user})
 
        socket.on('welcome',(data)=>{
+        setMessages([...messages,data])
+       
         console.log(data.user,data.message)
        })
 socket.on('userJoined',(data)=>{
+    setMessages([...messages,data])
 console.log(data.user,data.message)
 })
 socket.on('leave',(data)=>{
+    setMessages([...messages,data])
 console.log(data.user,data.message)
 })
 
@@ -44,12 +52,13 @@ return()=>{
 
    useEffect(()=>{
     socket.on('sendMessage',(data)=>{
+        setMessages([...messages,data])
 console.log(data.user,data.message,data.id)
     })
     return()=>{
-       
+       socket.off()
     }
-   },[])
+   },[messages])
 
     return (
         <div className='chatPage'>
@@ -61,7 +70,9 @@ console.log(data.user,data.message,data.id)
 
 
             </div>
-            <div className='chatBox'>  </div>
+            <ReactScrollTOBottom className='chatBox'> 
+            {messages.map((item,i)=><Message  user={item.id===id?'':item.user} message={item.message} classs={item.id===id?'right':'left'} ></Message>)}
+             </ReactScrollTOBottom>
 
           
             <div className='inputBox'>
