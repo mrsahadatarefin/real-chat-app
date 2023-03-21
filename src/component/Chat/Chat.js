17d32c8dd04a707f66570a6da3c034df3eb8449e
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-03-21 23:05:25 +0600

    working on

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {user} from "../Join/Join"
+import socketIo from 'socket.io-client'
+import './Chat.css'
+import sendLogo from '../../images/send.png'
+const ENDPOINR ='http://localhost:5000/'
 
+let socket;
 const Chat = () => {
+    const [id,setId]=useState('')
+
+    const send = ()=>{
+       const message= document.getElementById('chatInput').value;
+       document.getElementById('chatInput').value=""
+        socket.emit('message',{message,id});
+
+    }
+    
+    useEffect(()=>{
+
+        socket= socketIo(ENDPOINR,{transports:['websocket']})
+        socket.on('connect',()=>{
+            alert('connected')
+            setId(socket.id)
+        })
+        socket.emit('joined',{user})
+
+       socket.on('welcome',(data)=>{
+        console.log(data.user,data.message)
+       })
+socket.on('userJoined',(data)=>{
+console.log(data.user,data.message)
+})
+socket.on('leave',(data)=>{
+console.log(data.user,data.message)
+})
+
+return()=>{
+    socket.emit('disconnected')
+    socket.off();
+}
+    },[])
+
+
+   useEffect(()=>{
+    socket.on('sendMessage',(data)=>{
+console.log(data.user,data.message,data.id)
+    })
+    return()=>{
+       
+    }
+   },[])
+
     return (
-        <div>
-          {user}
+        <div className='chatPage'>
+    <div className='chatContainer'>
+
+        <div className='header'>
+            
+          
+
+
+            </div>
+            <div className='chatBox'>  </div>
+
+          
+            <div className='inputBox'>
+                <input type="text" id='chatInput' />
+                <button onClick={send} className='sendBtn'> <img src={sendLogo} alt=""  /></button>
+        </div>
+    </div>
         </div>
     );
 };
--- src/component/Join/Join.js
+++ src/component/Join/Join.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Join.css'
 import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom';
 
 let user;
-
+const sendUser = ()=>{
+    user = document.getElementById('joinInput').value;
+    document.getElementById('joinInput').value=''
+}
 
 const Join = () => {
-    const sendUser = ()=>{
-        user = document.getElementById('joinInput').value;
-        document.getElementById('joinInput').value=''
-    }
+   const [name,setName]=useState('') 
+    
     return (
         <div className='joinPage'>
            <div className='joinContainer'>
             <img src={logo} alt="logo" />
             <h1> SUPER CHAT </h1>
-            <input placeholder='enter your name' type="text" id='joinInput' />
+            <input onChange={(e)=>setName(e.target.value)} placeholder='enter your name' type="text" id='joinInput' />
  
-           <Link to='/chat'>
-           <button className='joinbtn' onClick={sendUser}>LOgin </button>
+           <Link to='/chat' onClick={(e)=>!name ? e.preventDefault(): null}>
+           <button   className='joinbtn' onClick={sendUser} >LOgin </button>
            </Link>
            
            </div>
